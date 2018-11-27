f4bc64a74178c4913a91cf8da2f35df098ff7335
Bertrand Lemasle <blemasle@gmail.com>
2018-11-27 20:50:59 +1300

    Reduced the size of gsm functions

--- src/SIM808.Gsm.cpp
+++ src/SIM808.Gsm.cpp
@@ -6,7 +6,6 @@ TOKEN_TEXT(CMGS, "+CMGS");
 
 bool SIM808::simUnlock(const char* pin)
 {
-	SENDARROW;
 	sendAT(SFP(TOKEN_CPIN), SFP(TOKEN_WRITE), pin);
 
 	return waitResponse(5000L) == 0;
@@ -14,7 +13,6 @@ bool SIM808::simUnlock(const char* pin)
 
 size_t SIM808::getSimState(char *state)
 {
-	SENDARROW;
 	sendAT(SFP(TOKEN_CPIN), SFP(TOKEN_READ));
 	if(waitResponse(5000L, SFP(TOKEN_CPIN)) != 0) return 0;
 	
@@ -26,7 +24,6 @@ size_t SIM808::getSimState(char *state)
 
 size_t SIM808::getImei(char *imei)
 {
-	SENDARROW;
 	sendAT(SF("+GSN"));
 
 	readNextLine(SIMCOMAT_DEFAULT_TIMEOUT);
@@ -39,17 +36,17 @@ size_t SIM808::getImei(char *imei)
 
 SIM808SignalQualityReport SIM808::getSignalQuality()
 {
-	SIM808SignalQualityReport report = {99, 99, 1};
-	SENDARROW;
-	sendAT(SFP(TOKEN_CSQ));
-	waitResponse(SFP(TOKEN_CSQ));
-
-	if (strstr_P(replyBuffer, TOKEN_CSQ) == 0) return report;
-
 	uint8_t quality;
 	uint8_t errorRate;
-	if (!parseReply(',', (uint8_t)SIM808_SIGNAL_QUALITY_RESPONSE::SIGNAL_STRENGTH, &quality) ||
-		!parseReply(',', (uint8_t)SIM808_SIGNAL_QUALITY_RESPONSE::BIT_ERROR_RATE, &errorRate)) return report;
+
+	SIM808SignalQualityReport report = {99, 99, 1};
+
+	sendAT(SFP(TOKEN_CSQ));
+	if(waitResponse(SFP(TOKEN_CSQ) != 0 ||
+		!parseReply(',', (uint8_t)SIM808_SIGNAL_QUALITY_RESPONSE::SIGNAL_STRENGTH, &quality) ||
+		!parseReply(',', (uint8_t)SIM808_SIGNAL_QUALITY_RESPONSE::BIT_ERROR_RATE, &errorRate) ||
+		waitResponse())
+		return report;
 
 	report.rssi = quality;
 	report.ber = errorRate;
@@ -60,23 +57,18 @@ SIM808SignalQualityReport SIM808::getSignalQuality()
 	else if (quality > 31) report.attenuation = 1;
 	else report.attenuation = map(quality, 2, 30, -110, -54);
 
-	waitResponse();
 	return report;
 }
 
 bool SIM808::setSmsMessageFormat(SIM808_SMS_MESSAGE_FORMAT format)
 {
-	SENDARROW;
 	sendAT(SF("+CMGF="), (uint8_t)format);
-
 	return waitResponse() == 0;
 }
 
 bool SIM808::sendSms(const char *addr, const char *msg)
 {
 	if (!setSmsMessageFormat(SIM808_SMS_MESSAGE_FORMAT::TEXT)) return false;
-
-	SENDARROW;
 	sendAT(SFP(TOKEN_CMGS), SFP(TOKEN_WRITE), SFP(TOKEN_QUOTE), addr, SFP(TOKEN_QUOTE));
 
 	if (!waitResponse(SF(">")) == 0) return false;
@@ -85,8 +77,6 @@ bool SIM808::sendSms(const char *addr, const char *msg)
 	print(msg);
 	print((char)0x1A);
 
-	if(!waitResponse(60000, SFP(TOKEN_CMGS)) == 0) return false;
-	if(!waitResponse() == 0) return false;
-
-	return true;
+	return waitResponse(60000L, SFP(TOKEN_CMGS)) == 0 &&
+		waitResponse())
 }
