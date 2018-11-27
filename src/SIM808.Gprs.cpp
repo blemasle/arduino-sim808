ff0564776dcd6e0c3f7fcf0ee2c7d7410a167825
Bertrand Lemasle <blemasle@gmail.com>
2018-11-28 11:41:23 +1300

    Using formatted commands for Gprs save some space

--- src/SIM808.Gprs.cpp
+++ src/SIM808.Gprs.cpp
@@ -1,7 +1,9 @@
 #include "SIM808.h"
 
-AT_COMMAND(SET_BEARER_SETTING, "AT+SAPBR=3,1,\"%S\",\"%s\"");
-AT_COMMAND(GPRS_START_TASK, "AT+CSTT=\"%s\",\"%s\",\"%s\"");
+AT_COMMAND(SET_BEARER_SETTING_PARAMETER, "+SAPBR=3,1,\"%S\",\"%s\"");
+AT_COMMAND(SET_BEARER_SETTING, "+SAPBR=%d,%d");
+AT_COMMAND(GPRS_START_TASK, "+CSTT=\"%s\",\"%s\",\"%s\"");
+AT_COMMAND(GPRS_ATTACH, "+CGATT=%d");
 
 AT_COMMAND_PARAMETER(BEARER, CONTYPE);
 AT_COMMAND_PARAMETER(BEARER, APN);
@@ -9,7 +11,6 @@ AT_COMMAND_PARAMETER(BEARER, USER);
 AT_COMMAND_PARAMETER(BEARER, PWD);
 
 TOKEN_TEXT(CGATT, "+CGATT");
-TOKEN_TEXT(SAPBR, "+SAPBR");
 TOKEN_TEXT(CIPSHUT, "+CIPSHUT");
 TOKEN_TEXT(CSTT, "+CSTT");
 TOKEN_TEXT(CIICR, "+CIICR");
@@ -18,7 +19,7 @@ TOKEN_TEXT(CGREG, "+CGREG");
 
 bool SIM808::setBearerSetting(Sim808ConstStr parameter, const char *value)
 {
-	sendFormatAT(SFP(AT_COMMAND_SET_BEARER_SETTING), parameter, value);
+	sendFormatAT(SFP(AT_COMMAND_SET_BEARER_SETTING_PARAMETER), parameter, value);
 	return waitResponse() == 0;
 }
 
@@ -45,23 +46,23 @@ bool SIM808::enableGprs(const char *apn)
 bool SIM808::enableGprs(const char *apn, const char* user, const char *password)
 {
 	return (sendAT(SFP(TOKEN_CIPSHUT)), waitResponse(65000L, SFP(TOKEN_SHUT_OK)) == 0) &&					//AT+CIPSHUT
-		(sendAT(SFP(TOKEN_CGATT), SFP(TOKEN_WRITE), 1), waitResponse(10000L) == 0) &&						//AT+CGATT=1
+		(sendFormatAT(SFP(AT_COMMAND_GPRS_ATTACH), 1), waitResponse(10000L) == 0) &&						//AT+CGATT=1
 
 		setBearerSetting(SFP(AT_COMMAND_PARAMETER_BEARER_CONTYPE), "GPRS") &&
 		setBearerSetting(SFP(AT_COMMAND_PARAMETER_BEARER_APN), apn) &&
 		(user == NULL || setBearerSetting(SFP(AT_COMMAND_PARAMETER_BEARER_USER), user)) &&
 		(password == NULL || setBearerSetting(SFP(AT_COMMAND_PARAMETER_BEARER_PWD), password)) &&
 
-		(sendFormatAT(SFP(AT_COMMAND_GPRS_START_TASK), apn, user, password), waitResponse() == 0) &&	//AT+CSTT="apn","user","password"
-		(sendAT(SFP(TOKEN_SAPBR), SFP(TOKEN_WRITE), 1, SFP(TOKEN_COMMA), 1), waitResponse(65000L) == 0) &&	//AT+SAPBR=1,1
+		(sendFormatAT(SFP(AT_COMMAND_GPRS_START_TASK), apn, user, password), waitResponse() == 0) &&		//AT+CSTT="apn","user","password"
+		(sendFormatAT(SFP(AT_COMMAND_SET_BEARER_SETTING), 1, 1), waitResponse(65000L) == 0) &&				//AT+SAPBR=1,1
 		(sendAT(SFP(TOKEN_CIICR)), waitResponse(65000L) == 0);												//AT+CIICR
 }
 
 bool SIM808::disableGprs()
 {
 	return (sendAT(SFP(TOKEN_CIPSHUT)), waitResponse(65000L, SFP(TOKEN_SHUT_OK)) == 0) &&					//AT+CIPSHUT
-		(sendAT(SFP(TOKEN_SAPBR), SFP(TOKEN_WRITE), 0, SFP(TOKEN_COMMA), 1), waitResponse(65000L) == 0) &&	//AT+SAPBR=0,1
-		(sendAT(SFP(TOKEN_CGATT), SFP(TOKEN_WRITE), 0), waitResponse(10000L) == 0);							//AT+CGATT=0
+		(sendFormatAT(SFP(AT_COMMAND_SET_BEARER_SETTING), 0, 1), waitResponse(65000L) == 0) &&				//AT+SAPBR=0,1
+		(sendFormatAT(SFP(AT_COMMAND_GPRS_ATTACH), 0), waitResponse(10000L) == 0);							//AT+CGATT=0
 }
 
 SIM808RegistrationStatus SIM808::getNetworkRegistrationStatus()
